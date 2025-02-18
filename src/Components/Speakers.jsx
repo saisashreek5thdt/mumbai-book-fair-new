587db6350a338ab27e3008d6c8bcac40c6c7cb76
Sai Sashreek D <saisashreekd@gmail.com>
2025-02-18 10:50:13 +0530

    Updated Page Content

--- src/Components/Speakers.jsx
+++ src/Components/Speakers.jsx
@@ -10,40 +10,47 @@ import speakerImg5 from "../assets/images/Speakers/Shiv_Khera.png";
 import speakerImg6 from "../assets/images/Speakers/Upendra_Rai.png";
 import speakerImg7 from "../assets/images/Speakers/VP_Speaker.png";
 import speakerImg8 from "../assets/images/Speakers/Ankur_Warikoo.png";
+import { Link } from "react-router-dom";
 
 export default function Speakers() {
+  const speakersPattern1 = {
+    backgroundImage: `url(${speakersPatternImg1})`,
+    backgroundSize: "cover",
+  };
 
-    const speakersPattern1 = {
-        backgroundImage: `url(${speakersPatternImg1})`,
-        backgroundSize: "cover",
-    }
+  const speakersPattern2 = {
+    backgroundImage: `url(${speakersPatternImg2})`,
+    backgroundSize: "cover",
+  };
 
-    const speakersPattern2 = {
-        backgroundImage: `url(${speakersPatternImg2})`,
-        backgroundSize: "cover",
-    }
-
-    const speakersPattern3 = {
-        backgroundImage: `url(${speakersPatternImg3})`,
-        backgroundSize: "cover",
-    }
+  const speakersPattern3 = {
+    backgroundImage: `url(${speakersPatternImg3})`,
+    backgroundSize: "cover",
+  };
 
   return (
     <>
-      <section className="speakers-one">
-        <div
-          className="speakers-one_pattern"
-          style={speakersPattern1}
-        ></div>
+      <section className="speakers-one" id="speakers">
+        <div className="speakers-one_pattern" style={speakersPattern1}></div>
         <div
           className="speakers-one_pattern-two"
           style={speakersPattern3}
         ></div>
         <div className="auto-container">
           <div className="sec-title title-anim centered">
-          <div className="sec-title_title" style={{ textTransform: "none" }}>Speakers Experts</div>
-          <h2 className="sec-title_heading" style={{ marginBottom: "15px", fontSize: "36px", fontWeight: "700" }}>Meet Speakers For Events</h2>
-
+            <div className="sec-title_title" style={{ textTransform: "none" }}>
+              Speakers Experts
+            </div>
+            <h2
+              className="sec-title_heading"
+              style={{
+                marginBottom: "15px",
+                fontSize: "36px",
+                fontWeight: "700",
+              }}
+            >
+              Meet Speakers For Events
+            </h2>
           </div>
           <div className="row clearfix">
             {/* Speaker Block One */}
@@ -59,7 +66,7 @@ export default function Speakers() {
                     className="speaker-block_one-pattern"
                     style={speakersPattern2}
                   ></div>
-                 {/* <div className="speaker-block_one-social">
+                  {/* <div className="speaker-block_one-social">
                     <span className="speaker-block_one-share fa-solid fa-share-nodes fa-fw"></span>
                     <div className="speaker-block_one-social-list">
                       <a
@@ -82,8 +89,9 @@ export default function Speakers() {
                     <a href="speaker-detail.html">Ankur Warikoo</a>
                   </h4>
 
-
-                  <div className="speaker-one_detail">Entrepreneur, Author, and Public Speaker</div>
+                  <div className="speaker-one_detail">
+                    Entrepreneur, Author, and Public Speaker
+                  </div>
                 </div>
               </div>
             </div>
@@ -123,7 +131,9 @@ export default function Speakers() {
                   <h4 className="speaker-one_title">
                     <a href="speaker-detail.html">Dr.Chandraprakash Dwivedi</a>
                   </h4>
-                  <div className="speaker-one_detail">Indian Film Director and Screenwriter</div>
+                  <div className="speaker-one_detail">
+                    Indian Film Director and Screenwriter
+                  </div>
                 </div>
               </div>
             </div>
@@ -164,7 +174,7 @@ export default function Speakers() {
                     <a href="speaker-detail.html">Govind Dholakia</a>
                   </h4>
                   <div className="speaker-one_detail">
-                  Founder and Chairman of Shree Ramkrishna Exports Pvt. Ltd
+                    Founder and Chairman of Shree Ramkrishna Exports Pvt. Ltd
                   </div>
                 </div>
               </div>
@@ -205,7 +215,9 @@ export default function Speakers() {
                   <h4 className="speaker-one_title">
                     <a href="speaker-detail.html">Kailash Kher</a>
                   </h4>
-                  <div className="speaker-one_detail">Indian Playback Singer and Music Composer</div>
+                  <div className="speaker-one_detail">
+                    Indian Playback Singer and Music Composer
+                  </div>
                 </div>
               </div>
             </div>
@@ -246,7 +258,7 @@ export default function Speakers() {
                     <a href="speaker-detail.html">Shiv Khera</a>
                   </h4>
                   <div className="speaker-one_detail">
-                  Author, Professional Speaker, and Motivational Trainer
+                    Author, Professional Speaker, and Motivational Trainer
                   </div>
                 </div>
               </div>
@@ -287,7 +299,9 @@ export default function Speakers() {
                   <h4 className="speaker-one_title">
                     <a href="speaker-detail.html">Upendra Rai</a>
                   </h4>
-                  <div className="speaker-one_detail">Senior Journalist and Author</div>
+                  <div className="speaker-one_detail">
+                    Senior Journalist and Author
+                  </div>
                 </div>
               </div>
             </div>
@@ -327,7 +341,9 @@ export default function Speakers() {
                   <h4 className="speaker-one_title">
                     <a href="speaker-detail.html">Vaibhav Purandare</a>
                   </h4>
-                  <div className="speaker-one_detail">Author and Journalist</div>
+                  <div className="speaker-one_detail">
+                    Author and Journalist
+                  </div>
                 </div>
               </div>
             </div>
@@ -367,8 +383,25 @@ export default function Speakers() {
                   <h4 className="speaker-one_title">
                     <a href="speaker-detail.html">Ankur Warikoo</a>
                   </h4>
-                  <div className="speaker-one_detail">Entrepreneur, Author, and Public Speaker</div>
+                  <div className="speaker-one_detail">
+                    Entrepreneur, Author, and Public Speaker
+                  </div>
                 </div>
+              </div>
+            </div>
+
+            <div className="col-lg-12 col-md-12 col-sm-12 text-center">
+              <div className="about-one_button">
+                <Link
+                  to="/speakers"
+                  className="theme-btn btn-style-one"
+                  target="_parent"
+                >
+                  <span className="btn-wrap">
+                    <span className="text-one">More Speakers</span>
+                    <span className="text-two">More Speakers</span>
+                  </span>
+                </Link>
               </div>
             </div>
           </div>
