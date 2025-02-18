e98b44fbba8469ba502d4a3f66e0c2b43ccbf851
Rakeshvanam6868 <https://github.com/Rakeshvanam6868>
2025-02-18 14:13:10 +0530

    Added Language icon

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -59,6 +59,18 @@ export default function Header() {
                       <li>
                         <Link to="/">FAQ</Link>
                       </li>
+                      <li className="dropdown language-icon">
+                    {/* <img src={language} alt="" /> */}
+                    <span>
+                    <i className="fa fa-language fa-2xl" style={{width:"30px",height:"30px", color:"gold"}}></i>
+                    {/* <LiaLanguageSolid style={{width:"30px",height:"30px", color:"white"}}/> */}
+                     </span>
+                          <ul>
+                            <li><a href="">English</a></li>
+                            <li><a href="">Hindi</a></li>
+                            <li><a href="">Marathi</a></li>
+                          </ul>
+                    </li>
                     </ul>
                   </div>
                 </nav>
