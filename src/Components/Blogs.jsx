8ed270340baf85097a8f40dbdd2ce170293deca5
Sai Sashreek D <saisashreekd@gmail.com>
2025-02-19 11:02:40 +0530

    Updated with Swiper Package

--- src/Components/Blogs.jsx
+++ src/Components/Blogs.jsx
@@ -1,15 +1,62 @@
 import { Link } from "react-router-dom";
-import "owl.carousel/dist/assets/owl.carousel.css";
-import "owl.carousel/dist/assets/owl.theme.default.css";
-import OwlCarousel from "react-owl-carousel";
 import blogImg1 from "../assets/images/resource/volunteer.png";
 import blogImg2 from "../assets/images/resource/exhibitor.png";
 import blogImg3 from "../assets/images/resource/visitor.png";
 import blogImg4 from "../assets/images/resource/speaker.png";
 import blogImg5 from "../assets/images/resource/vendor.png";
 import blogImg6 from "../assets/images/resource/sponsor.png";
+import blogImg7 from "../assets/images/resource/author.png";
+import "swiper/css";
+import "swiper/css/pagination";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, Pagination } from "swiper/modules";
 
 export default function Blogs() {
+  const registerInfo = [
+    {
+      id: 1,
+      title: "Volunteer",
+      link: "https://docs.google.com/forms/d/e/1FAIpQLSePmHdusFo3-doXpJRmMxpmT9aqVweuzQhqn_OGJGcTdV-mLQ/viewform",
+      imgURL: blogImg1,
+    },
+    {
+      id: 2,
+      title: "Exhibitor",
+      link: "https://docs.google.com/forms/d/e/1FAIpQLSePmHdusFo3-doXpJRmMxpmT9aqVweuzQhqn_OGJGcTdV-mLQ/viewform",
+      imgURL: blogImg2,
+    },
+    {
+      id: 3,
+      title: "Visitor",
+      link: "https://docs.google.com/forms/d/e/1FAIpQLSfJ6YcEuTFVIB-wCflitmtfX1minVEGO4KrOPR8Fc26vPM7XA/viewform",
+      imgURL: blogImg3,
+    },
+    {
+      id: 4,
+      title: "Speaker",
+      link: "https://docs.google.com/forms/d/e/1FAIpQLSehWMPA1WSMIC1u54WHSlPRifEf1aP2PF_UBQahAI-SnbqLBw/viewform",
+      imgURL: blogImg4,
+    },
+    {
+      id: 5,
+      title: "Vendor",
+      link: "https://docs.google.com/forms/d/e/1FAIpQLSe_CBsGMOh5NU6IgrGabtPLCGwpRhZGZ8erNCpYAcl4Q1TVAg/viewform",
+      imgURL: blogImg5,
+    },
+    {
+      id: 6,
+      title: "Sponsor",
+      link: "https://docs.google.com/forms/d/e/1FAIpQLScIajlb9neewUiqRpH6KrDXYsVKT7QXdc1Ioif8frcyuhsm8Q/viewform",
+      imgURL: blogImg6,
+    },
+    {
+      id: 7,
+      title: "Author",
+      link: "https://docs.google.com/forms/d/e/1FAIpQLSehWMPA1WSMIC1u54WHSlPRifEf1aP2PF_UBQahAI-SnbqLBw/viewform",
+      imgURL: blogImg7,
+    },
+  ];
+
   return (
     <>
       <section className="news-one" id="register">
@@ -23,166 +70,44 @@ export default function Blogs() {
             </div>
           </div>
 
-          {/* Owl Carousel Slider - FIXED */}
-          <OwlCarousel
-            className="testimonial-slider owl-theme wow fadeInUp"
-            data-wow-delay=".25s"
-            loop
-            margin={10}
-            nav
-            dots={true}
-            autoplay={true}
-            autoplayTimeout={3000}
-            smartSpeed={1000}
-            responsive={{
-              0: { items: 1 },
-              600: { items: 2 },
-              1000: { items: 3 },
+          <Swiper
+            className="swiper"
+            slidesPerView={3}
+            spaceBetween={20}
+            loop={true}
+            autoplay={{
+              delay: 3000, // Adjust delay time (3 seconds here)
+              disableOnInteraction: false,
+            }}
+            modules={[Autoplay, Pagination]}
+            breakpoints={{
+              320: { slidesPerView: 1 }, // Mobile view shows 1 slide
+              768: { slidesPerView: 3 }, // Tablet view shows 2 slides
+              1024: { slidesPerView: 3 }, // Desktop view shows 3 slides
             }}
           >
-            {/* News Block Items */}
-            <div className="news-block_one">
-              <div className="news-block_one-inner">
-                <div className="news-block_one-tag">Registration</div>
-                <div className="news-block_one-image">
-                  <Link to="/">
-                    <img src={blogImg1} alt="" />
-                  </Link>
-                  <img src={blogImg1} alt="" />
-                </div>
-                <div className="news-block_one-content">
-                  <div className="news-block_one-date">1-5 April</div>
-                  <h5 className="news-block_one-title">
-                    <Link to="/">Volunteer</Link>
-                  </h5>
-                  <a
-                    className="news-block_one-more"
-                    href="https://docs.google.com/forms/d/e/1FAIpQLSePmHdusFo3-doXpJRmMxpmT9aqVweuzQhqn_OGJGcTdV-mLQ/viewform"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    Register
-                  </a>
-                </div>
-              </div>
-            </div>
-
-            <div className="news-block_one">
-              <div className="news-block_one-inner">
-                <div className="news-block_one-tag">Registration</div>
-                <div className="news-block_one-image">
-                  <Link to="/">
-                    <img src={blogImg2} alt="" />
-                  </Link>
-                  <img src={blogImg2} alt="" />
-                </div>
-                <div className="news-block_one-content">
-                  <div className="news-block_one-date">1-5 April</div>
-                  <h5 className="news-block_one-title">
-                    <Link to="/">Exhibitor</Link>
-                  </h5>
-                  <a
-                    className="news-block_one-more"
-                    href="https://docs.google.com/forms/d/e/1FAIpQLSePmHdusFo3-doXpJRmMxpmT9aqVweuzQhqn_OGJGcTdV-mLQ/viewform"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    Register
-                  </a>
-                </div>
-              </div>
-            </div>
-
-            <div className="news-block_one">
-              <div className="news-block_one-inner">
-                <div className="news-block_one-tag">Registration</div>
-                <div className="news-block_one-image">
-                  <Link to="/">
-                    <img src={blogImg3} alt="" />
-                  </Link>
-                  <img src={blogImg3} alt="" />
-                </div>
-                <div className="news-block_one-content">
-                  <div className="news-block_one-date">1-5 April</div>
-                  <h5 className="news-block_one-title">
-                    <Link to="/">Visitor</Link>
-                  </h5>
-                  <a
-                    className="news-block_one-more"
-                    href="https://docs.google.com/forms/d/e/1FAIpQLSfJ6YcEuTFVIB-wCflitmtfX1minVEGO4KrOPR8Fc26vPM7XA/viewform"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    Register
-                  </a>
-                </div>
-              </div>
-            </div>
-
-            <div className="news-block_one">
-              <div className="news-block_one-inner">
-                <div className="news-block_one-tag">Registration</div>
-                <div className="news-block_one-image">
-                  <Link to="/">
-                    <img src={blogImg4} alt="" />
-                  </Link>
-                  <img src={blogImg4} alt="" />
-                </div>
-                <div className="news-block_one-content">
-                  <div className="news-block_one-date">1-5 April</div>
-                  <h5 className="news-block_one-title">
-                    <Link to="/">Speaker</Link>
-                  </h5>
-                  <a
-                    className="news-block_one-more"
-                    href="https://docs.google.com/forms/d/e/1FAIpQLSehWMPA1WSMIC1u54WHSlPRifEf1aP2PF_UBQahAI-SnbqLBw/viewform"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    Register
-                  </a>
-                </div>
-              </div>
-            </div>
-
-            <div className="news-block_one">
-              <div className="news-block_one-inner">
-                <div className="news-block_one-tag">Registration</div>
-                <div className="news-block_one-image">
-                  <Link to="/">
-                    <img src={blogImg5} alt="" />
-                  </Link>
-                  <img src={blogImg5} alt="" />
-                </div>
-                <div className="news-block_one-content">
-                  <div className="news-block_one-date">1-5 April</div>
-                  <h5 className="news-block_one-title">
-                    <Link to="/">Vendor</Link>
-                  </h5>
-                  <a
-                    className="news-block_one-more"
-                    href="https://docs.google.com/forms/d/e/1FAIpQLSe_CBsGMOh5NU6IgrGabtPLCGwpRhZGZ8erNCpYAcl4Q1TVAg/viewform"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    Register
-                  </a>
-                </div>
-              </div>
-            </div>
-
-            <div className="news-block_one">
-              <div className="news-block_one-inner">
-                <div className="news-block_one-tag">Registration</div>
-                <div className="news-block_one-image">
-                  <Link to="/">
-                    <img src={blogImg6} alt="" />
-                  </Link>
-                  <img src={blogImg6} alt="" />
-                </div>
-                <div className="news-block_one-content">
-                  <div className="news-block_one-date">1-5 April</div>
-                  <h5 className="news-block_one-title">
+            {registerInfo.map((event, index) => (
+              <SwiperSlide key={index}>
+                <div className="news-block_one">
+                  <div className="news-block_one-inner">
+                    <div className="news-block_one-tag">
+                      <Link
+                        to={event.link}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        Registration
+                      </Link>
+                    </div>
+                    <div className="news-block_one-image">
+                      <Link to="/">
+                        <img src={event.imgURL} alt="" />
+                      </Link>
+                      <img src={event.imgURL} alt="" />
+                    </div>
+                    <div className="news-block_one-content">
+                      <div className="news-block_one-date">{event.title}</div>
+                      {/* <h5 className="news-block_one-title">
                     <Link to="/">Sponsor</Link>
                   </h5>
                   <a
@@ -192,11 +117,13 @@ export default function Blogs() {
                     rel="noopener noreferrer"
                   >
                     Register
-                  </a>
+                  </a> */}
+                    </div>
+                  </div>
                 </div>
-              </div>
-            </div>
-          </OwlCarousel>
+              </SwiperSlide>
+            ))}
+          </Swiper>
         </div>
       </section>
     </>
