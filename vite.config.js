e466cbb516f5357186d67823cd927379ab880f06
Vishal Payyawar <vishal.payyawar22@vit.edu>
2025-02-19 12:16:14 +0530

    add new file

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,26 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   optimizeDeps: {
-//     include: ["react-router-dom"]
-//   },
-//   plugins: [react()],
-// })
-
-
-
-
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/",  // set the base patth
   optimizeDeps: {
     include: ["react-router-dom"]
   },
   plugins: [react()],
 })
+
+
+
+
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   base: "/",  // set the base patth
+//   optimizeDeps: {
+//     include: ["react-router-dom"]
+//   },
+//   plugins: [react()],
+// })
