c920cc47b1c486f61a86728128ed6c1572d4171b
Rakeshvanam6868 <https://github.com/Rakeshvanam6868>
2025-02-21 13:11:32 +0530

    Added search functionality in publisher page

---
+++ src/Components/Publishers/PublisherData.js
@@ -0,0 +1,76 @@
+import authorImg1 from "../../assets/images/publishers/publisher.png";
+
+export const publications = [
+    {
+      boothNumber: 1,
+      imgURL: authorImg1,
+      name: "Publisher One",
+      link: "/publishers",
+    },
+    {
+      boothNumber: 2,
+      imgURL: authorImg1,
+      name: "Publisher Two",
+      link: "/publishers",
+    },
+    {
+      boothNumber: 3,
+      imgURL: authorImg1,
+      name: "Publisher Three",
+      link: "/publishers",
+    },
+    {
+      boothNumber: 4,
+      imgURL: authorImg1,
+      name: "Publisher Four",
+      link: "/publishers",
+    },
+    {
+      boothNumber: 5,
+      imgURL: authorImg1,
+      name: "Publisher Five",
+      link: "/publishers",
+    },
+    {
+      boothNumber: 6,
+      imgURL: authorImg1,
+      name: "Publisher Six",
+      link: "/publishers",
+    },
+    {
+      boothNumber: 7,
+      imgURL: authorImg1,
+      name: "Publisher Seven",
+      link: "/publishers",
+    },
+    {
+      boothNumber: 8,
+      imgURL: authorImg1,
+      name: "Publisher Eight",
+      link: "/publishers",
+    },
+    {
+      boothNumber: 9,
+      imgURL: authorImg1,
+      name: "Publisher Nine",
+      link: "/publishers",
+    },
+    {
+      boothNumber: 10,
+      imgURL: authorImg1,
+      name: "Publisher Ten",
+      link: "/publishers",
+    },
+    {
+      boothNumber: 11,
+      imgURL: authorImg1,
+      name: "Publisher Eleven",
+      link: "/publishers",
+    },
+    {
+      boothNumber: 12,
+      imgURL: authorImg1,
+      name: "Publisher Twelve",
+      link: "/publishers",
+    },
+  ];
--- src/Components/Publishers/PublishersInfo.jsx
+++ src/Components/Publishers/PublishersInfo.jsx
@@ -1,118 +1,147 @@
-import authorImg1 from "../../assets/images/publishers/publisher.png";
+import { publications } from "./PublisherData";
+import { useState } from "react";
+import "../../publisher.css";
 
 export default function PublishersInfo() {
-  const publications = [
-    {
-      boothNumber: 1,
-      imgURL: authorImg1,
-      name: "Publisher Name",
-      link: "/publishers",
-    },
-    {
-      boothNumber: 2,
-      imgURL: authorImg1,
-      name: "Publisher Name",
-      link: "/publishers",
-    },
-    {
-      boothNumber: 3,
-      imgURL: authorImg1,
-      name: "Publisher Name",
-      link: "/publishers",
-    },
-    {
-      boothNumber: 4,
-      imgURL: authorImg1,
-      name: "Publisher Name",
-      link: "/publishers",
-    },
-    {
-      boothNumber: 5,
-      imgURL: authorImg1,
-      name: "Publisher Name",
-      link: "/publishers",
-    },
-    {
-      boothNumber: 6,
-      imgURL: authorImg1,
-      name: "Publisher Name",
-      link: "/publishers",
-    },
-    {
-      boothNumber: 7,
-      imgURL: authorImg1,
-      name: "Publisher Name",
-      link: "/publishers",
-    },
-    {
-      boothNumber: 8,
-      imgURL: authorImg1,
-      name: "Publisher Name",
-      link: "/publishers",
-    },
-    {
-      boothNumber: 9,
-      imgURL: authorImg1,
-      name: "Publisher Name",
-      link: "/publishers",
-    },
-    {
-      boothNumber: 10,
-      imgURL: authorImg1,
-      name: "Publisher Name",
-      link: "/publishers",
-    },
-    {
-      boothNumber: 11,
-      imgURL: authorImg1,
-      name: "Publisher Name",
-      link: "/publishers",
-    },
-    {
-      boothNumber: 12,
-      imgURL: authorImg1,
-      name: "Publisher Name",
-      link: "/publishers",
-    },
-  ];
+  const [searchInput, setSearchInput] = useState("");
+  const [filteredPublications, setFilteredPublications] = useState([]);
+  const [isSearched, setIsSearched] = useState(false);
+  const [suggestions, setSuggestions] = useState([]); // State to store suggestions
+
+  // Function to handle search and filter
+  const handleSearch = () => {
+    const filtered = publications.filter((res) =>
+      res.name.toLowerCase().includes(searchInput.toLowerCase())
+    );
+    setFilteredPublications(filtered);
+    setIsSearched(true);
+    setSuggestions([]); // Clear suggestions after search
+  };
+
+  // Function to clear the search and reset to default state
+  const handleClearSearch = () => {
+    setSearchInput("");
+    setFilteredPublications([]);
+    setIsSearched(false);
+    setSuggestions([]); // Clear suggestions
+  };
+
+  // Function to update suggestions dynamically
+  const handleInputChange = (e) => {
+    const inputValue = e.target.value;
+    setSearchInput(inputValue);
+
+    if (inputValue.trim() === "") {
+      setSuggestions([]); // Clear suggestions if input is empty
+    } else {
+      // Filter suggestions based on input
+      const matchedSuggestions = publications
+        .filter((pub) => pub.name.toLowerCase().includes(inputValue.toLowerCase()))
+        .map((pub) => pub.name); // Extract only names for suggestions
+      setSuggestions(matchedSuggestions);
+    }
+  };
+
+  // Function to handle suggestion selection
+  const handleSuggestionClick = (suggestion) => {
+    setSearchInput(suggestion); // Set the selected suggestion as the search input
+    setSuggestions([]); // Clear suggestions
+
+    // Perform search with the selected suggestion
+    const filtered = publications.filter((res) =>
+      res.name.toLowerCase().includes(suggestion.toLowerCase())
+    );
+    setFilteredPublications(filtered);
+    setIsSearched(true);
+  };
 
   return (
     <>
       <section className="testimonial-two">
         <div className="auto-container">
+          {/* Search Input */}
+          <div className="search-container">
+            <div className="search-input">
+              <input
+                type="text"
+                value={searchInput}
+                placeholder="Search publishers..."
+                onChange={handleInputChange} // Update input and suggestions
+                className=""
+              />
+              {/* Suggestions Dropdown */}
+              {suggestions.length > 0 && (
+                <ul className="suggestions-dropdown">
+                  {suggestions.map((suggestion, index) => (
+                    <li
+                      key={index}
+                      onClick={() => handleSuggestionClick(suggestion)}
+                      className="suggestion-item"
+                    >
+                      {suggestion}
+                    </li>
+                  ))}
+                </ul>
+              )}
+              {/* Clear Search Button */}
+              {isSearched && (
+                <button className="clear-search-button" onClick={handleClearSearch}>
+                  X
+                </button>
+              )}
+            </div>
+
+            <button className="search-button" onClick={handleSearch}>
+              Search
+            </button>
+          </div>
+
+          {/* Display Results or All Publications */}
           <div className="row clearfix">
-            {publications.map((publish, index) => (
-              <div
-                key={index}
-                className="news-block_two col-lg-3 col-md-6 col-sm-12"
-              >
+            {(isSearched ? filteredPublications : publications).map(
+              (publish, index) => (
                 <div
-                  className="news-block_two-inner wow fadeInLeft"
-                  data-wow-delay="0ms"
-                  data-wow-duration="1500ms"
+                  key={index}
+                  className="news-block_two col-lg-3 col-md-6 col-sm-12"
                 >
-                  {/* Post Info */}
-                  <div className="news-block_two-date">
-                    Booth Number: {publish.boothNumber}
-                  </div>
-                  <div className="news-block_two-image">
-                    <a href={publish.link}>
+                  <div
+                    className="news-block_two-inner wow fadeInLeft"
+                    data-wow-delay="0ms"
+                    data-wow-duration="1500ms"
+                  >
+                    {/* Post Info */}
+                    <div className="news-block_two-date">
+                      Booth Number: {publish.boothNumber}
+                    </div>
+                    <div className="news-block_two-image">
+                      <a href={publish.link}>
+                        <img src={publish.imgURL} alt="" />
+                      </a>
                       <img src={publish.imgURL} alt="" />
-                    </a>
-                    <img src={publish.imgURL} alt="" />
-                  </div>
-                  <div className="news-block_two-content">
-                    <h4 className="news-block_two-title">
-                      <a href={publish.link}>{publish.name}</a>
-                    </h4>
-                    <a className="news-block_two-more" href={publish.link}>
-                      Publishings
-                    </a>
+                    </div>
+                    <div className="news-block_two-content">
+                      <h4 className="news-block_two-title">
+                        <a href={publish.link}>{publish.name}</a>
+                      </h4>
+                      <a className="news-block_two-more" href={publish.link}>
+                        Publishings
+                      </a>
+                    </div>
                   </div>
                 </div>
-              </div>
-            ))}
+              )
+            )}
           </div>
+
+          {/* Show Message if No Results Found After Search */}
+          {isSearched && filteredPublications.length === 0 && (
+            <div className="no-results">
+              No matching publishers found.{" "}
+              <button className="clear-search-link" onClick={handleClearSearch}>
+                Back to all publishers
+              </button>
+            </div>
+          )}
 
           {/* Styled Pagination */}
           <ul className="styled-pagination text-center">
