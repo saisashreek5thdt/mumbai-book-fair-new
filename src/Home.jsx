440b1a110cb4a3f0fc7ee6ec46189974c9d82d24
Vishal Payyawar <vishal.payyawar22@vit.edu>
2025-02-21 17:03:22 +0530

    modification of innerpage redirect to home page

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,3 +1,125 @@
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import {
+  Header,
+  Slider,
+  AboutUs,
+  Counter,
+  Speakers,
+  Features,
+  Events,
+  Gallery,
+  Pricing,
+  VideoSection,
+  Clients,
+  Blogs,
+  Newsletter,
+  Footer,
+  FaqSection,
+} from "./Components/index";
+
+export default function Home() {
+  const location = useLocation();
+
+  // Handle smooth scrolling to the section when the page loads with a hash
+  useEffect(() => {
+    if (location.hash) {
+      // Wait for the DOM to fully load
+      setTimeout(() => {
+        const element = document.querySelector(location.hash);
+        if (element) {
+          element.scrollIntoView({ behavior: "smooth" });
+        }
+      }, 100); // Small delay to ensure the DOM is ready
+    }
+  }, [location]);
+
+  return (
+    <>
+      <div className="hidden-bar-wrapper">
+        <div className="page-wrapper">
+          {/* Preloader Start */}
+          <div className="preloader"></div>
+          {/* Preloader End */}
+
+          {/* Header Starts */}
+          <Header />
+          {/* Header Ends */}
+
+          {/* Slider Section Start */}
+          <Slider />
+          {/* Slider Section End */}
+
+          {/* About Section Start */}
+          <section id="about">
+            <AboutUs />
+          </section>
+          {/* About Section End */}
+
+          {/* Counter Section Start */}
+          <Counter />
+          {/* Counter Section End */}
+
+          {/* Features Start */}
+          <Features />
+          {/* Features End */}
+
+          {/* Speakers Section Start */}
+          <section id="speakers">
+            <Speakers />
+          </section>
+          {/* Speakers Section End */}
+
+          {/* Events Section Start */}
+          <section id="event">
+            <Events />
+          </section>
+          {/* Events Section End */}
+
+          {/* Gallery Start */}
+          <Gallery />
+          {/* Gallery End */}
+
+          {/* Pricing Start */}
+          <Pricing />
+          {/* Pricing End */}
+
+          {/* Video Section Start */}
+          <VideoSection />
+          {/* Video Section End */}
+
+          {/* Clients Start */}
+          <Clients />
+          {/* Clients End */}
+
+          {/* FAQ Section Start */}
+          <section id="faq">
+            <FaqSection />
+          </section>
+          {/* FAQ Section End */}
+
+          {/* Blogs Start */}
+          <Blogs />
+          {/* Blogs End */}
+
+          {/* Newsletter Start */}
+          <Newsletter />
+          {/* Newsletter End */}
+
+          {/* Footer Start */}
+          <Footer />
+          {/* Footer End */}
+        </div>
+      </div>
+    </>
+  );
+}
+
+
+
+//original code 
+
+
 // import {
 //   Header,
 //   Slider,
@@ -74,101 +196,3 @@
 //     </>
 //   );
 // }
-
-
-
-import {
-  Header,
-  Slider,
-  AboutUs,
-  Counter,
-  Speakers,
-  Features,
-  Events,
-  Gallery,
-  Pricing,
-  VideoSection,
-  Clients,
-  Blogs,
-  Newsletter,
-  Footer,
-  FaqSection
-} from "./Components/index";
-
-export default function Home() {
-  return (
-    <>
-      <div className="hidden-bar-wrapper">
-        <div className="page-wrapper">
-          {/* Preloader Start */}
-          <div className="preloader"></div>
-          {/* Preloader End */}
-
-          {/* Header Starts */}
-          <Header />
-          {/* Header Ends */}
-
-          {/* Slider Section Start */}
-          <Slider />
-          {/* Slider Section End */}
-
-          {/* About Section Start */}
-          <section id="about">
-            <AboutUs />
-          </section>
-          {/* About Section End */}          
-
-          {/* Counter Section Start */}
-          <Counter />
-          {/* Counter Section End */}
-
-          {/* Features Start */}
-          <Features />
-          {/* Features End */}
-
-          {/* Speakers Section Start */}
-          <section id="speakers">
-            <Speakers />
-          </section>
-          {/* Speakers Section End */}          
-
-          {/* Events Section Start */}
-          <Events />
-          {/* Events Section End */}
-
-          {/* Gallery Start */}
-          <Gallery />
-          {/* Gallery End */}
-
-          {/* Pricing Start */}
-          <Pricing />
-          {/* Pricing End */}
-
-          {/* Video Section Start */}
-          <VideoSection />
-          {/* Video Section End */}
-
-          {/* Clients Start */}
-          <Clients />
-          {/* Clients End */}
-
-          {/* FAQ Section Start */}
-          <FaqSection />
-          {/* FAQ Section End */}
-
-          {/* Blogs Start */}
-          <Blogs />
-          {/* Blogs End */}          
-
-          {/* Newletter Start */}
-          <Newsletter />
-          {/* Newletter End */}
-
-          {/* Footer Start */}
-          <Footer />
-          {/* Footer End */}
-        </div>
-      </div>
-    </>
-  );
-}
