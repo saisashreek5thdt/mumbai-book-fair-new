f30dd8868ba8ac87db504832577a299f88c2804d
Sai Sashreek D <saisashreekd@gmail.com>
2025-02-27 17:52:17 +0530

    New Team Component Updated

--- src/Components/TeamExecutive.jsx
+++ src/Components/TeamExecutive.jsx
@@ -16,16 +16,17 @@ export default function TeamExecutive() {
             <div className="inner-box">
               {/* <div className="icon-box">
                   <span className="icon flaticon-megaphone-1"></span>
-                </div> */}
+                </div> */}                
               <div className="speaker-block">
+              <div className="news-block_two-date">CHIEF CONVENOR - MUMBAI BOOK FESTIVAL</div>
                 <img src={speakerImg1} alt="" />
               </div>
               <h4>
                 <Link to="/">Acharya Pawan Tripathi</Link>
               </h4>
               <div className="text">
-                Author, Columnist, Social Worker and Trustee of Sri Sidhi
-                Vinayak Temple Trust, SSVT.
+                Author, columnist, social worker, and Treasurer of the Sri
+                Siddhi Vinayak Temple Trust (SSVT), Mumbai.
               </div>
               {/* <div className="btn-box">
                 <Link to="/" className="theme-btn btn-style-one">
@@ -48,14 +49,18 @@ export default function TeamExecutive() {
                   <span className="icon flaticon-arrows"></span>
                 </div> */}
               <div className="speaker-block">
+              <div className="news-block_two-date">CHIEF PATRON - PROF MILIN</div>
                 <img src={speakerImg2} alt="" />
               </div>
               <h4>
-                <Link to="/">Sri Mohit Kamboj Bhartiya</Link>
+                <Link to="/">Prof. Milind Sudhakar Marathe</Link>
               </h4>
               <div className="text">
                 Social Worker, Entrepreneur and Champion of{" "}
-                <strong>&quot;<em>Har Hath Ek Kitab</em>&quot;</strong>.
+                <strong>
+                  &quot;<em>Har Hath Ek Kitab</em>&quot;
+                </strong>
+                .
               </div>
               {/* <div className="btn-box">
                 <Link to="/" className="theme-btn btn-style-one">
@@ -68,35 +73,6 @@ export default function TeamExecutive() {
             </div>
           </div>
 
-          {/* Feature Block */}
-          <div
-            className="feature-block col-lg-4 col-md-6 col-sm-12 wow fadeInUp"
-            data-wow-delay="800ms"
-          >
-            <div className="inner-box">
-              {/* <div className="icon-box">
-                  <span className="icon flaticon-deal"></span>
-                </div> */}
-              <div className="speaker-block">
-                <img src={speakerImg1} alt="" />
-              </div>
-              <h4>
-                <Link to="/">Acharya Pawan Tripathi</Link>
-              </h4>
-              <div className="text">
-                Author, Columnist, Social Worker and Trustee of Sri Sidhi
-                Vinayak Temple Trust, SSVT.
-              </div>
-              {/* <div className="btn-box">
-                <Link to="/" className="theme-btn btn-style-one">
-                  <span className="btn-wrap">
-                    <span className="text-one">Read More</span>
-                    <span className="text-two">Read More</span>
-                  </span>
-                </Link>
-              </div> */}
-            </div>
-          </div>
         </div>
       </div>
     </>
---
+++ src/Components/TeamExecutive2.jsx
@@ -0,0 +1,68 @@
+import { Link } from "react-router-dom";
+import speakerImg1 from "../assets/images/team/Acharya.png";
+import speakerImg2 from "../assets/images/team/Milin.png";
+
+export default function TeamExecutive2() {
+  return (
+    <>
+      <div className="row clearfix">
+        {/* Team Block Start */}
+        <div className="news-block_two col-lg-4 col-md-6 col-sm-12">
+          <div
+            className="news-block_two-inner wow fadeInLeft"
+            data-wow-delay="0ms"
+            data-wow-duration="1500ms"
+          >
+            {/* Team Info Start */}
+            <div className="news-block_two-date">
+              CHIEF CONVENOR
+            </div>
+            <div className="news-block_two-image">
+              <Link to="/">
+                <img src={speakerImg1} alt="Speaker Image" />
+              </Link>
+              <img src={speakerImg1} alt="Speaker Image" />
+            </div>
+            <div className="news-block_two-content">
+              <h4 className="news-block_two-title">Acharya Pawan Tripathi</h4>
+              <div className="text">
+                Author, columnist, social worker, and Treasurer of the Sri
+                Siddhi Vinayak Temple Trust (SSVT), Mumbai.
+              </div>
+            </div>
+            {/* Team Info End */}
+          </div>
+        </div>
+        {/* Team Block End */}
+        {/* Team Block Start */}
+        <div className="news-block_two col-lg-4 col-md-6 col-sm-12">
+          <div
+            className="news-block_two-inner wow fadeInLeft"
+            data-wow-delay="0ms"
+            data-wow-duration="1500ms"
+          >
+            {/* Team Info Start */}
+            <div className="news-block_two-date">
+            CHIEF PATRON
+            </div>
+            <div className="news-block_two-image">
+              <Link to="/">
+                <img src={speakerImg2} alt="Speaker Image" />
+              </Link>
+              <img src={speakerImg2} alt="Speaker Image" />
+            </div>
+            <div className="news-block_two-content">
+              <h4 className="news-block_two-title">Prof. Milind Sudhakar Marathe</h4>
+              <div className="text">
+                Author, columnist, social worker, and Treasurer of the Sri
+                Siddhi Vinayak Temple Trust (SSVT), Mumbai.
+              </div>
+            </div>
+            {/* Team Info End */}
+          </div>
+        </div>
+        {/* Team Block End */}
+      </div>
+    </>
+  );
+}
